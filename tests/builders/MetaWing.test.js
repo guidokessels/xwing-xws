5d9654575e888e9a8ea734ce186a0e0b8faacaa8
Guido Kessels <guidokessels@gmail.com>
2017-09-08 16:39:40 +0200

    feat(): add support for MetaWing /squad_visualizations url
    Example: http://meta-wing.com/squad_visualizations/4695526

--- tests/builders/MetaWing.test.js
+++ tests/builders/MetaWing.test.js
@@ -1,15 +1,21 @@
 import MetaWing from '../../lib/builders/MetaWing';
 
 describe('#matches() matches all MetaWing urls', () => {
-  test('http url', () => {
+  test('/squadrons url', () => {
     const builder = new MetaWing();
     const url = 'http://meta-wing.com/squadrons/123';
 
     expect(builder.matches(url)).toBe(true);
   });
-  test('url without protocol', () => {
+  test('/squadrons url without protocol', () => {
     const builder = new MetaWing();
     const url = 'meta-wing.com/squadrons/123';
+
+    expect(builder.matches(url)).toBe(true);
+  });
+  test('/squad_visualisations url', () => {
+    const builder = new MetaWing();
+    const url = 'http://meta-wing.com/squad_visualizations/4695526';
 
     expect(builder.matches(url)).toBe(true);
   });
